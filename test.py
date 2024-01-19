51de65cc8d7d51106781b3d799fd5fdcba379e03
Adrenokrome72 <103280846+Adrenokrome72@users.noreply.github.com>
2024-01-19 15:53:55 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,67 @@
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+        
+    def rate_lecturer(self,mentor,course,grade):
+        if isinstance(lector, Lecturer) and course in lector.courses_attached and course in self.courses_in_progress:
+            if course in lector.grades:
+                lector.grades[course] += [grade]
+            else:
+                lector.grades[course] = [grade]   
+                
+    def __str__(self):
+        
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+class Lecturer(Mentor):
+    def __init__(self,name,surname,grades_lecture):
+        super.__init__()
+
+    def __str__(self):
+        
+    
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super.__init__()
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+        
+    def __str__(self):
+        
+        
+
+
+
+
+
+
+
+
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
+
+cool_mentor = Mentor('Some', 'Buddy')
+cool_mentor.courses_attached += ['Python']
+
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+
+print(best_student.grades)
