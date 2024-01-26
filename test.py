73bcf5ae651658a532764a15c8a636dd85331b72
Adrenokrome72 <103280846+Adrenokrome72@users.noreply.github.com>
2024-01-26 16:54:32 +0300

    Update test.py

--- test.py
+++ test.py
@@ -6,16 +6,30 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
-        
-    def rate_lecturer(self,mentor,course,grade):
+    
+    #метод выставления оценки лекторам    
+    def rate_lecturer(self, mentor, course, grade):
         if isinstance(lector, Lecturer) and course in lector.courses_attached and course in self.courses_in_progress:
             if course in lector.grades:
                 lector.grades[course] += [grade]
             else:
-                lector.grades[course] = [grade]   
-                
-    def __str__(self):
+                lector.grades[course] = [grade]
+        else:
+            return 'Ошибка'
         
+    #функция для подсчета средней оценки за домашние задания по всем студентам в рамках конкретного курса
+    def average_student_grades(self):
+        sum_grades = 0
+        len_grades = 0
+        for list_grades in self.grades.values():
+            sum_grades += sum(list_grades)
+            len_grades += len(list_grades)
+        result = round((sum_grades / len_grades), 2)
+    
+    #перегрузка класса student           
+    def __str__(self):
+        result = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {average_student_grades()}\nКурсы в процессе изучения{", ".join(self.courses_in_progress)}\nЗаверешенные курсы: {", ".join(self.finished_courses)}' 
+        return result
 
 class Mentor:
     def __init__(self, name, surname):
@@ -24,17 +38,30 @@ class Mentor:
         self.courses_attached = []
 
 class Lecturer(Mentor):
-    def __init__(self,name,surname,grades_lecture):
-        super.__init__()
+    def __init__(self, name, surname):
+        super.__init__(name, surname)
+        self.grades = {}
 
+    #функция для подсчета средней оценки за лекции всех лекторов в рамках курса
+    def awerage_lecturer_grades():
+        sum_grades = 0
+        len_grades = 0
+        for list_grades in self.grades.values():
+            sum_grades += sum(list_grades)
+            len_grades += len(list_grades)
+        result = round((sum_grades / len_grades), 2)
+        return result
+        
+    #перегрузка класса lecturer
     def __str__(self):
+        result = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {average_student_grades()}' 
+        return result    
         
     
 
 class Reviewer(Mentor):
-    def __init__(self, name, surname):
-        super.__init__()
-    def rate_hw(self, student, course, grade):
+    #метод выставления оценки студентам от ревьюеров    
+    def rate_student_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
                 student.grades[course] += [grade]
@@ -42,10 +69,11 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
-        
+    #перегрузка класса reviewer    
     def __str__(self):
+        result = f'Имя: {self.name}\nФамилия: {self.surname}' 
+        return result        
         
-        
 
 
 
@@ -54,14 +82,14 @@ class Reviewer(Mentor):
 
 
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
+#best_student = Student('Ruoy', 'Eman', 'your_gender')
+#best_student.courses_in_progress += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+#cool_mentor = Mentor('Some', 'Buddy')
+#cool_mentor.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+#cool_mentor.rate_hw(best_student, 'Python', 10)
+#cool_mentor.rate_hw(best_student, 'Python', 10)
+#cool_mentor.rate_hw(best_student, 'Python', 10)
 
-print(best_student.grades)
+#print(best_student.grades)
